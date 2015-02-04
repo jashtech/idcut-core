315219c2e0e878301a344b14695edbf33f962e09
pjaworski_jash <pjaworski@jash.pl>
2015-02-04 20:33:25 +0100

    Started converting to PaymentModule: there is no Validation and payment controllers, and missing one (commented hook) Signed-off-by:pjaworski_jash <pjaworski@jash.pl>

--- kickass.php
+++ kickass.php
@@ -11,8 +11,12 @@ class Kickass extends Module {
         $this->author = 'Tomasz Wesołowski <twesolowski@jash.pl>';
         $this->tab = 'front_office_features';
         $this->version = '1.0';
-        $this->controllers = array('auth');
+        $this->controllers = array('auth', 'payment', 'validation');
+        $this->is_eu_compatible = 1;
 
+        $this->currencies = true;
+        $this->currencies_mode = 'checkbox';
+        
         $this->bootstrap = true;
         parent::__construct();
 
@@ -21,14 +25,24 @@ class Kickass extends Module {
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 
         $this->core = require_once 'bootstrap/prestashop.php';
+
+//        $c_tmp = $this->getConfigFieldsValues();
+        $i = 1;
+        if ((!isset($c_tmp['PS_KICKASS_CLIENT_ID']) || !isset($c_tmp['PS_KICKASS_REDIRECT_URL']) || empty($c_tmp['PS_KICKASS_CLIENT_ID']) || empty($c_tmp['PS_KICKASS_REDIRECT_URL'])))
+                $this->warning .= '<br>' . $i++ . '. ' . $this->l('The client ID and Redirect url fields must be configured before using this module.');
+        if ((!isset($c_tmp['PS_KICKASS_CLIENT_SECRET']) || empty($c_tmp['PS_KICKASS_CLIENT_SECRET'])))
+                $this->warning .= '<br>' . $i++ . '. ' . $this->l('You need to authorize your shop with API before using this module.');
+        if (!count(Currency::checkPaymentCurrencies($this->id)))
+                $this->warning .= '<br>' . $i++ . '. ' . $this->l('No currency has been set for this module.');
     }
 
     public function install()
     {
         if (!parent::install() ||
-                !Configuration::updateValue('PS_KICKASS_SCOPES', 'id;name') || 
-                !$this->installTab()
-        )
+                !Configuration::updateValue('PS_KICKASS_SCOPES', 'id;name') ||
+                !$this->installTab() ||
+                !$this->registerHook('payment') || ! $this->registerHook('displayPaymentEU') //|| !$this->registerHook('paymentReturn')
+        ) 
         {
             return false;
         }
@@ -41,7 +55,7 @@ class Kickass extends Module {
                 !Configuration::deleteByName('PS_KICKASS_CLIENT_ID') ||
                 !Configuration::deleteByName('PS_KICKASS_CLIENT_SECRET') ||
                 !Configuration::deleteByName('PS_KICKASS_REDIRECT_URL') ||
-                !Configuration::deleteByName('PS_KICKASS_SCOPES') || 
+                !Configuration::deleteByName('PS_KICKASS_SCOPES') ||
                 !$this->uninstallTab()
         )
         {
@@ -49,18 +63,18 @@ class Kickass extends Module {
         }
         return true;
     }
-    
+
     public function installTab()
     {
-            $tab = new Tab();
-            $tab->active = 1;
-            $tab->class_name = 'AdminKickass';
-            $tab->name = array();
-            foreach (Language::getLanguages(true) as $lang)
-                    $tab->name[$lang['id_lang']] = 'Deal settings';
-            $tab->id_parent = 0;
-            $tab->module = $this->name;
-            return $tab->add();
+        $tab = new Tab();
+        $tab->active = 1;
+        $tab->class_name = 'AdminKickass';
+        $tab->name = array();
+        foreach (Language::getLanguages(true) as $lang)
+            $tab->name[$lang['id_lang']] = 'Deal settings';
+        $tab->id_parent = 0;
+        $tab->module = $this->name;
+        return $tab->add();
     }
 
     public function uninstallTab()
@@ -68,50 +82,49 @@ class Kickass extends Module {
             $id_tab = (int)Tab::getIdFromClassName('AdminKickass');
             if ($id_tab)
             {
-                    $tab = new Tab($id_tab);
-                    return $tab->delete();
+            $tab = new Tab($id_tab);
+            return $tab->delete();
             }
             else
-                    return false;
+            return false;
     }
-    
+
     public function getContent2()
     {
             if (Tools::isSubmit('submitModule'))
             {
 
-                /* Update Client ID */
-                $clientId = Tools::getValue('PS_KICKASS_CLIENT_ID');
+            /* Update Client ID */
+            $clientId = Tools::getValue('PS_KICKASS_CLIENT_ID');
                 if (isset($clientId))
                 {
-                    $this->core->config()->update("PS_KICKASS_CLIENT_ID", $clientId);
-                }
+                $this->core->config()->update("PS_KICKASS_CLIENT_ID", $clientId);
+            }
 
-                /* Update Client Secret */
-                $clientSecret = trim(Tools::getValue('PS_KICKASS_CLIENT_SECRET'));
+            /* Update Client Secret */
+            $clientSecret = trim(Tools::getValue('PS_KICKASS_CLIENT_SECRET'));
                 if (!empty($clientSecret))
                 {
-                    $this->core->config()->setEncrypted("PS_KICKASS_CLIENT_SECRET", $clientSecret);
-                }
+                $this->core->config()->setEncrypted("PS_KICKASS_CLIENT_SECRET", $clientSecret);
+            }
 
-                /* Update redirect URL */
-                $redirectUrl = Tools::getValue('PS_KICKASS_REDIRECT_URL');
+            /* Update redirect URL */
+            $redirectUrl = Tools::getValue('PS_KICKASS_REDIRECT_URL');
                 if (isset($redirectUrl))
                 {
-                    $this->core->config()->update("PS_KICKASS_REDIRECT_URL", $redirectUrl);
-                }
-                $updated_info = 1;
+                $this->core->config()->update("PS_KICKASS_REDIRECT_URL", $redirectUrl);
             }
+            $updated_info = 1;
+        }
             Tools::redirectAdmin($this->context->link->getAdminLink('AdminKickass').'&updated_info='.(isset($updated_info)?$updated_info:0));
     }
-    
+
     public function getContent()
     {
         $html = '';
 
-        if (Tools::isSubmit('submitModule'))
-        {
-            $this->getContent2();
+        if (Tools::isSubmit('submitModule')) {
+//            $this->getContent2();
             /* Update Client ID */
             $clientId = Tools::getValue('PS_KICKASS_CLIENT_ID');
             if (isset($clientId))
@@ -206,6 +219,73 @@ class Kickass extends Module {
         $html .= $this->core->config()->getEncrypted("PS_KICKASS_API_TOKEN");;
         return $html;
     }
+    
+    
+    
+    public function hookPayment($params)
+    {
+            if (!$this->active)
+                    return;
+            if (!$this->checkCurrency($params['cart']))
+                    return;
+
+            $this->smarty->assign(array(
+                    'this_path' => $this->_path,
+                    'this_path_kickass' => $this->_path,
+                    'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
+            ));
+            return $this->display(__FILE__, 'payment.tpl');
+    }
+
+    public function hookDisplayPaymentEU($params)
+    {
+            if (!$this->active)
+                    return;
+            if (!$this->checkCurrency($params['cart']))
+                    return;
+
+            return array(
+                    'cta_text' => $this->l('Pay by Kickass'),
+                    'logo' => null,
+                    'action' => $this->context->link->getModuleLink($this->name, 'validation', array(), true)
+            );
+    }
+
+//    public function hookPaymentReturn($params)
+//    {
+//            if (!$this->active)
+//                    return;
+//
+//            $state = $params['objOrder']->getCurrentState();
+//            if (in_array($state, array(Configuration::get('PS_OS_CHEQUE'), Configuration::get('PS_OS_OUTOFSTOCK'), Configuration::get('PS_OS_OUTOFSTOCK_UNPAID'))))
+//            {
+//                    $this->smarty->assign(array(
+//                            'total_to_pay' => Tools::displayPrice($params['total_to_pay'], $params['currencyObj'], false),
+//                            'chequeName' => $this->chequeName,
+//                            'chequeAddress' => Tools::nl2br($this->address),
+//                            'status' => 'ok',
+//                            'id_order' => $params['objOrder']->id
+//                    ));
+//                    if (isset($params['objOrder']->reference) && !empty($params['objOrder']->reference))
+//                            $this->smarty->assign('reference', $params['objOrder']->reference);
+//            }
+//            else
+//                    $this->smarty->assign('status', 'failed');
+//            return $this->display(__FILE__, 'payment_return.tpl');
+//    }
+    
+    public function checkCurrency($cart)
+    {
+            $currency_order = new Currency((int)($cart->id_currency));
+            $currencies_module = $this->getCurrency((int)$cart->id_currency);
+
+            if (is_array($currencies_module))
+                    foreach ($currencies_module as $currency_module)
+                            if ($currency_order->id == $currency_module['id_currency'])
+                                    return true;
+            return false;
+    }
+    
 
     public function getConfigFieldsValues()
     {
---
+++ views/templates/hook/payment.tpl
@@ -0,0 +1,5 @@
+<p class="payment_module">
+	<a href="{$link->getModuleLink('kickass', 'payment', [], true)|escape:'html'}" title="{l s='Pay by Kickass' mod='kickass'}">
+		{l s='Pay by Kickass' mod='kickass'}
+	</a>
+</p>
