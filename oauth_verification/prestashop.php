32dcb046b1695774985ba2b59805b7b9c1398a0c
Tomasz Wesołowski <twesolowski@jash.pl>
2015-02-18 08:13:37 +0100

    oauth verification page

--- oauth_verification/prestashop.php
+++ oauth_verification/prestashop.php
@@ -1,5 +1,4 @@
 <?php
-
 include(dirname(__FILE__) . '/../../../config/config.inc.php');
 $core = require_once dirname(__FILE__) . '/../bootstrap/prestashop.php';
 
@@ -7,14 +6,11 @@ $code = Tools::getValue("code");
 $state = Tools::getValue("state");
 $savedState = $core->config()->getEncrypted("PS_KICKASS_OAUTH_STATE");
 
-if (!$code)
-{
+if (!$code) {
     exit('Invalid code');
-} elseif (empty($state) || ($state !== $savedState))
-{
+} elseif (empty($state) || ($state !== $savedState)) {
     exit('Invalid state');
-} else
-{
+} else {
     $token = $core->getOAuthProvider()->getAccessToken('authorization_code', [
         'code' => $code
     ]);
@@ -23,3 +19,14 @@ if (!$code)
 }
 
 $core->config()->deleteByName("PS_KICKASS_OAUTH_STATE");
+?><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>OAuth2 verification</title>
+
+    </head>
+    <body>
+
+    </body>
+</html>
