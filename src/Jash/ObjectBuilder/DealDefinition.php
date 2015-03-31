fd8725f2b9d1e5435af7e6eb5d4d735015a614c6
Tomasz Wesołowski <twesolowski@jash.pl>
2015-03-31 13:01:28 +0200

    DealDefinition builder and object files

---
+++ src/Jash/ObjectBuilder/DealDefinition.php
@@ -0,0 +1,18 @@
+<?php
+
+class DealDefinition{
+    private $dealDefinition;
+    public function __construct()
+    {
+
+    }
+
+    public function singleFromJSON($json){
+        
+    }
+
+    public function fromJSON($json){
+        
+    }
+
+}
---
+++ src/Jash/ObjectBuilder/JSONBuilder.php
@@ -0,0 +1,11 @@
+<?php
+namespace Kickass\Jash\ObjectBuilder;
+
+class JSONBuilder{
+    private $dealDefinition;
+
+
+
+    public function
+
+}
