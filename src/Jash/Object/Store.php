131a6c41bec4636cffa6b51f7db8ef9eb997c8a6
Tomasz Wesolowski <twesolowski@jash.pl>
2015-05-29 11:59:38 +0200

    Blank store object

---
+++ src/Jash/Object/Store.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace IDcut\Jash\Store;
+
+class Store
+{
+    private $name = null;
+    private $url = null;
+    private $payment_return_url = null;
+    private $hook_url = null;
+    private $join_deal_url = null;
+
+    
+    
+}
